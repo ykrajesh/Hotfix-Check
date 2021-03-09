74159f07d47cc4c9f9ec76e36478f731586b72fc
Rajesh Yadav <76614478+ykrajesh@users.noreply.github.com>
2021-03-09 22:30:30 +0530

    Add files via upload
    upload code

---
+++ patch-check.ps1
@@ -0,0 +1,7 @@
+﻿$hostname = Get-Content C:\hotfix\hostname.txt
+foreach ($name in $hostname)
+{
+  Get-HotFix -ComputerName $name | Select-Object HotFixID , InstalledOn , PSComputerName ,Description -Last 3
+# $deatil |  Export-Csv C:\hotfix1.csv -NoTypeInformation -Append
+ 
+}
